421ffb92b117494386081c66a2a5afacb4d938fa
Demsci <644386347@qq.com>
2023-05-13 23:52:18 -0700

    更新load.sh

--- load.sh
+++ load.sh
@@ -2,7 +2,7 @@
 
 projectname=$1
 
-cp -ar ./project/public/uboot/* ./idh
-cp -ar ./project/my_project/${projectname} ./idh
+cp -ar ./project/public/uboot/* ./idh/uboot
+cp -ar ./project/my_project/${projectname}/* ./idh/uboot
 
 echo -e "\033[32m load file success!\033[0m"
